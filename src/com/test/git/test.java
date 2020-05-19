91d51b1a35977fbd8a49dde77685fc73e37f3bc0
JuetEm <sbkyo88@gmail.com>
2020-05-19 15:55:15 +0900

    커밋을 하겠소! MAC

--- src/com/test/git/test.java
+++ src/com/test/git/test.java
@@ -5,6 +5,8 @@ public class test {
     public static void main(String[] args){
 
         System.out.println("Working? 이거이거 잘 모르니까 영 불편하구만?!");
+
+        System.out.println("어라!? 이거 좀 적응이 되 갈 것 같기도 하고!");
     }
 
 }
