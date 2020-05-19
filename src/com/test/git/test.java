f337abaabc56e67fe7ef738525d7e44d7b0f9e1a
JuetEm <sbkyo88@gmail.com>
2020-05-19 15:52:12 +0900

    커밋 테스트1

--- src/com/test/git/test.java
+++ src/com/test/git/test.java
@@ -3,7 +3,8 @@ package com.test.git;
 public class test {
 
     public static void main(String[] args){
-        System.out.println("Working?");
+
+        System.out.println("Working? 이거이거 잘 모르니까 영 불편하구만?!");
     }
 
 }
